29323ed82bcf36e6b6b68b7f53dc9fed0d482bf8
Eric Thomas <eathom91@gmail.com>
2022-05-22 00:19:07 -0400

    Fixed compilation errors

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -3,14 +3,14 @@ import {Product} from "../product.model";
 
 // Component for rendering all ProductRows and storing the currently selected Product.
 @Component({
-  selector: 'app-products-list',
+  selector: 'products-list',
   templateUrl: './products-list.component.html',
 })
 export class ProductsListComponent {
   // These will be implemented soon...
-  // TODO tomorrow Sun 
-  @Input() productList: Product[];
+  // TODO tomorrow Sun
+  @Input() productList!: Product[];
 
-  @Output() onProductSelected: EventEmitter<Product>;
+  @Output() onProductSelected!: EventEmitter<Product>;
 
 }
