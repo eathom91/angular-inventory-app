69b5ada2ab35be500fc3fdb718007504f6e1b492
eric <eathom91@gmail.com>
2022-05-26 22:05:59 -0400

    Added new component logic

--- src/app/price-display/price-display.component.ts
+++ src/app/price-display/price-display.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'price-display',
-  templateUrl: './price-display.component.html',
+  template: `
+  <div class="price-display">\${{price}}</div>
+  `,
   styleUrls: ['./price-display.component.css']
 })
 export class PriceDisplayComponent implements OnInit {
+  @Input() price!: Number;
 
   constructor() { }
 
